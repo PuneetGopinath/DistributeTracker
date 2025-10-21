87b3daee94ff8e484ca88f03e33c7289fc1eb65b
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-21 10:40:01 +0530

    feat: implement auth routing, use signedIn state for displaying links appropriately and implement 404 route

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,32 +5,43 @@
  * License: MIT (see LICENSE)
  */
 
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import Layout from "./components/Layout";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+
 import CreateItem from "./components/Dashboard/CreateItem";
 import ListItems from "./components/Dashboard/ListItems";
 import LogTransaction from "./components/Dashboard/LogTransaction";
 import ViewTransactions from "./components/Dashboard/ViewTransactions";
 
 export default function App() {
+    const [ isSignedIn, setIsSignedIn ] = useState(localStorage.getItem("signedin") === "true");
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
+                <Route path="/" element={<Layout signedIn={isSignedIn} />}>
                     <Route index element={<Home />} />
+                    <Route path="signin" element={<SignIn signedIn={isSignedIn} setSigned={setIsSignedIn} />} />
+                    <Route path="signup" element={<SignUp signedIn={isSignedIn} setSigned={setIsSignedIn} />} />
                     <Route path="dashboard">
                         <Route path="items">
-                            <Route index element={<ListItems />} />
-                            <Route path="create" element={<CreateItem />} />
+                            <Route index element={<ListItems signedIn={isSignedIn}/>} />
+                            <Route path="create" element={<CreateItem signedIn={isSignedIn} />} />
                         </Route>
                         <Route path="transactions">
-                            <Route index element={<ViewTransactions />} />
-                            <Route path="log" element={<LogTransaction />} />
+                            <Route index element={<ViewTransactions signedIn={isSignedIn} />} />
+                            <Route path="log" element={<LogTransaction signedIn={isSignedIn} />} />
                         </Route>
                     </Route>
                 </Route>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
